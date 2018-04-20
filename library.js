be64e469b2e416dbdfb44570f00e9f62d5817219
jcarter0149 <jacobraycarter@gmail.com>
2018-04-20 11:34:39 -0500

    JC added his books

---
+++ library.js
@@ -0,0 +1,64 @@
+const firstBook = Object.create({}, {
+    Title: {
+        enumerable: true, 
+        writable: true, 
+        value: 'Killers of the Flower Moon'  
+    }, 
+    Author: {
+        enumerable: true, 
+        writable: true, 
+        value: 'David Grann'  
+    }, 
+    Genre: {
+        enumerable: true, 
+        writable: true, 
+        value: '20th Century U.S. History'  
+    }, 
+    ISBN: {
+        enumerable: true, 
+        writable: true, 
+        value: 9780307742483  
+    }, 
+    checkedOut: {
+        enumerable: true, 
+        writable: true, 
+        value: false  
+    }, 
+    dueDate: {
+        enumerable: true, 
+        writable: true, 
+        value: ''  
+    }
+})
+const secondBook = Object.create({}, {
+    Title: {
+        enumerable: true, 
+        writable: true, 
+        value: 'Greeks Bearing Gifts'  
+    }, 
+    Author: {
+        enumerable: true, 
+        writable: true, 
+        value: 'Philip Kerr'  
+    }, 
+    Genre: {
+        enumerable: true, 
+        writable: true, 
+        value: 'Suspense & Thriller'  
+    }, 
+    ISBN: {
+        enumerable: true, 
+        writable: true, 
+        value: 9780399177064  
+    }, 
+    checkedOut: {
+        enumerable: true, 
+        writable: true, 
+        value: false  
+    }, 
+    dueDate: {
+        enumerable: true, 
+        writable: true, 
+        value: ''  
+    }
+})
