064594dff2daf5e0b033e5f4e9f056c5fbf1b78d
jcarter0149 <jacobraycarter@gmail.com>
2018-04-20 15:38:34 -0500

    tried the checkout fun

--- checkoutFun.js
+++ checkoutFun.js
@@ -5,21 +5,47 @@ const yyyy = today.getFullYear();
 today = (mm + "/" + dd + "/" + yyyy)
 const fortnite = new Date(Date.now() + 12096e5)
 
-function checkOut(Title){
-    let matchingBook;
-    let bookDB = library;
-    let matchingBooks = bookDB.filter((book) => {
-        return book.Title === library.Title;
-    }
-);
-    if (matchingBooks.length === 1) {
-        matchingBook = matchingBooks [0];
-}
-    if (matchingBook && !matchingBook.checkedOut){
-        matchingBook.checkedOut = true;
-        this.bookshelf.push(matchingBook);
-        return this.bookshelf && fortnite;
-    }else {
-        return null;
-    }
-}
+const findBookshelf = id => {
+    let patronBookshelf
+    patrons.forEach(
+        patron => {
+            if (patron.ID === id)
+            patronBookshelf = patron.bookshelf
+        }
+    )
+    return patronBookshelf
+ }
+ 
+ const findBook = title => {
+    let book
+    library.forEach(
+        currentBook => {
+            if (book.Title === title)
+            book === currentBook
+        }
+    )
+    return book
+ }
+ 
+ function checkout(id, title) {
+ 
+    patronBookshelf(id).push(foundBook(title))
+ }
+
+
+    // let matchingBook;
+//     let matchingBooks = library.filter((book) => {
+//         return book.Title === library.Title;
+//     }
+// );
+//     if (matchingBooks.length === 1) {
+//         matchingBook = matchingBooks [0];
+// }
+//     if (matchingBook && !matchingBook.checkedOut){
+//         matchingBook.checkedOut = true;
+//         this.bookshelf.push(matchingBook);
+//         return this.bookshelf && fortnite;
+//     }else {
+//         return null;
+    // }
+
