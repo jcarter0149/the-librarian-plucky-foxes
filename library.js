4c6ec2b43a5483c9ad55b6e1df0d4ce96aba62a3
joshdbarton <joshdbarton@gmail.com>
2018-04-20 12:21:15 -0500

    combined and deleted extra files

--- library.js
+++ library.js
@@ -56,6 +56,34 @@ const library = [
         ISBN: 9780399177064,
         checkedOut: false,
         dueDate: ""
-    }
+    },
+    {
+        Title: " Let's explore diabetes with owls: Essays, Etc",
+        Author: "David Sedaris",
+        Genre: "Humor",
+        ISBN:  9780316154697,
+        checkedOut: false,
+        dueDate: ''
+    },
+    
+    {
+        Title: " A game of thrones: A Song of Ice and Fire Series, Book 1",
+        Author: "George R. R. Martin",
+        Genre: "Science Fiction",
+        ISBN:9780553103540  ,
+        checkedOut: false,
+        dueDate: ''
+    },
+    
+    {
+        Title: " God bless you, Mr. Rosewater",
+        Author: "Kurt Vonnegut",
+        Genre: "Science Fiction",
+        ISBN: 9780385333474,
+        checkedOut: false,
+        dueDate: ''
+    }, 
 
 ]
+
+localStorage.setItem('library', JSON.stringify(library))
