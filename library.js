e3c849b49c5bef74aa85ef5ea653f9e230d60ca2
joshdbarton <joshdbarton@gmail.com>
2018-04-20 12:06:17 -0500

    combined databases

--- library.js
+++ library.js
@@ -1,111 +1,61 @@
-// const bookFactory = (Title, Author, Genre, ISBN, checkoutOut, dueDate) => {
-//     const newBook = Object.create ({}, {
-//         Title,
-//         Author,
-//         Genre,
-//         ISBN,
-//         checkOut, 
-//         dueDate
-//     })
-//     library.push(newBook)
-// } 
+const library = [
+    {
+        Title: "Guns, Germs, and Steel",
+        Author: "Jeremy Diamond",
+        Genre: "Anthropology",
+        ISBN:  9781586638634,
+        checkedOut: false,
+        dueDate: ''
+    },
+    {
+        Title: "Jurassic Park",
+        Author: "Michael Crichton",
+        Genre: "Science Fiction",
+        ISBN:  9780784838068,
+        checkedOut: false,
+        dueDate: ''
+    },
+    
+    {
+        Title: "Ender's Game",
+        Author: "Orson Scott Card",
+        Genre: "Science Fiction",
+        ISBN: 0312932081,
+        checkedOut: false,
+        dueDate: ''
+    }, 
+    {
+        Title: "Accidentlly Married",
+        Author: "R.R. Banks",
+        Genre: "Romance",
+        ISBN: 1987558936,
+        checkedOut: false,
+        dueDate: ""
+    },
+    {
+        Title: "The House At Pooh Corner",
+        Author: "A.A. Milne",
+        Genre: "Children\'s literature",
+        ISBN: 0140361227, 
+        checkedOut: false,
+        dueDate: ""  
+    },  
+    {
+        Title: 'Killers of the Flower Moon',
+        Author: ' David Grann',
+        Genre: '20th Century U.S. History',
+        ISBN: 9780307742483,
+        checkedOut: false,
+        dueDate: ""
+    },
 
-
-
-const myBook = Object.create({}, {
-    Title: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Guns, Germs, and Steel'  
-    }, 
-    Author: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Jeremy Diamond'  
-    }, 
-    Genre: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Anthropology'  
-    }, 
-    ISBN: {
-        enumerable: true, 
-        writable: true, 
-        value: 9781586638634  
-    }, 
-    checkedOut: {
-        enumerable: true, 
-        writable: true, 
-        value: false  
-    }, 
-    dueDate: {
-        enumerable: true, 
-        writable: true, 
-        value: ''  
+    {
+        Title: 'Greeks Bearing Gifts',
+        Author: 'Philip Kerr',
+        Genre: 'Suspense & Thrillers',
+        ISBN: 9780399177064,
+        checkedOut: false,
+        dueDate: ""
     }
-})
 
-const firstBook = Object.create({}, {
-    Title: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Killers of the Flower Moon'  
-    }, 
-    Author: {
-        enumerable: true, 
-        writable: true, 
-        value: 'David Grann'  
-    }, 
-    Genre: {
-        enumerable: true, 
-        writable: true, 
-        value: '20th Century U.S. History'  
-    }, 
-    ISBN: {
-        enumerable: true, 
-        writable: true, 
-        value: 9780307742483  
-    }, 
-    checkedOut: {
-        enumerable: true, 
-        writable: true, 
-        value: false  
-    }, 
-    dueDate: {
-        enumerable: true, 
-        writable: true, 
-        value: ''  
-    }
-})
-const secondBook = Object.create({}, {
-    Title: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Greeks Bearing Gifts'  
-    }, 
-    Author: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Philip Kerr'  
-    }, 
-    Genre: {
-        enumerable: true, 
-        writable: true, 
-        value: 'Suspense & Thriller'  
-    }, 
-    ISBN: {
-        enumerable: true, 
-        writable: true, 
-        value: 9780399177064  
-    }, 
-    checkedOut: {
-        enumerable: true, 
-        writable: true, 
-        value: false  
-    }, 
-    dueDate: {
-        enumerable: true, 
-        writable: true, 
-        value: ''  
-    }
-})
+]
