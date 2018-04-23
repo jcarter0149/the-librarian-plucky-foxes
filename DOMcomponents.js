17ee6852bd7df2fcab4b097936498a1542c36ca9
joshdbarton <joshdbarton@gmail.com>
2018-04-23 11:30:42 -0500

    added fee and title element if statements to printer function

--- DOMcomponents.js
+++ DOMcomponents.js
@@ -1,15 +1,19 @@
-buildElement = (tag, content) => {
+const buildElement = (tag, content) => {
     const element = document.createElement([tag])
     element.textContent = content
     transactionArticle.appendChild(element)
 }
 
-const printTransaction = (Transaction, Patron, Title, Fee) => {
+const printTransaction = (transaction, patron, title, fee) => {
     const transactionArticle = document.createElement('article')
-    buildElement('h3', `Transaction: ${Transaction}`)
-    buildElement('p', `Patron: ${Patron}`)
-    buildElement('p',  `Title: ${Title}`)
-    buildElement('p', `Fee: ${Fee}`)
+    buildElement('h3', `Transaction: ${transaction}`)
+    buildElement('p', `Patron: ${patron}`)
+    if (transaction === 'Checkout' || transaction === "Return") {
+        buildElement('p',  `Title: ${title}`)
+    }
+    if fee {
+        buildElement('p', 'Fee: $5.00')
+    }
     body.appendChild(transactionArticle)
 }
 
