0f3d001f7cc54a729a35f09c49f632c10d145bf8
jcarter0149 <jacobraycarter@gmail.com>
2018-04-23 09:55:27 -0500

    added the checkout fun

--- checkoutFun.js
+++ checkoutFun.js
@@ -5,47 +5,30 @@ const yyyy = today.getFullYear();
 today = (mm + "/" + dd + "/" + yyyy)
 const fortnite = new Date(Date.now() + 12096e5)
 
-const findBookshelf = id => {
-    let patronBookshelf
-    patrons.forEach(
-        patron => {
-            if (patron.ID === id)
-            patronBookshelf = patron.bookshelf
-        }
+
+const checkout = (id, title) => {
+    const bookshelf = findBookshelf(id)
+    const book = findBook([title])
+    if (library.title === bookshelf.title)
+    alert(`This book is currently checked out. It is due back ${fortnite}`
     )
-    return patronBookshelf
- }
- 
- const findBook = title => {
-    let book
-    library.forEach(
-        currentBook => {
-            if (book.Title === title)
-            book === currentBook
-        }
-    )
-    return book
- }
- 
- function checkout(id, title) {
- 
-    patronBookshelf(id).push(foundBook(title))
- }
+    else (library.title !== bookshelf.title)
+    library.dueDate = fortnite
+    library.checkedOut = true
+    bookshelf.push(library.title)
+}
 
 
-    // let matchingBook;
-//     let matchingBooks = library.filter((book) => {
-//         return book.Title === library.Title;
-//     }
-// );
-//     if (matchingBooks.length === 1) {
-//         matchingBook = matchingBooks [0];
+// const findBookshelf = (id) => {
+//     const patron = patrons.find(patron => patron.ID === id)
+//     const patronBookshelf = patron.bookshelf
+//     return patronBookshelf
 // }
-//     if (matchingBook && !matchingBook.checkedOut){
-//         matchingBook.checkedOut = true;
-//         this.bookshelf.push(matchingBook);
-//         return this.bookshelf && fortnite;
-//     }else {
-//         return null;
-    // }
+
+// const findBook = title => {
+//     const book = library.find(currentBook => book.Title === title)
+//     return book
+}
+
+
 
