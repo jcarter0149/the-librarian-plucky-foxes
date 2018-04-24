15ce1749424fbcc196541af6347ca878f21be076
joshdbarton <joshdbarton@gmail.com>
2018-04-24 15:38:33 -0500

    fixed return books function

--- returnbooks.js
+++ returnbooks.js
@@ -2,11 +2,15 @@
 let returnCheckedBook = (ID, title) => {
     let bookTitle = findBook(title);
     let bookShelf = findBookshelf(ID);
-    const fee = (Date.parse(bookTitle.dueDate)) < (Date.parse(new Date()))
-    bookTitle.checkedOut = false;
-    bookTitle.dueDate = " ";
-    bookShelf.pop(findBookOnBookshelf(ID, title))
-    printTransaction("Return", findPatron(ID), bookTitle.Title, fee)
-    saveLibrary()
-    savePatrons()
+    if (!bookTitle.checkedOut) {
+        alert('Book not checked out!')
+    } else {
+        const fee = (Date.parse(bookTitle.dueDate)) < (Date.parse(new Date()))
+        bookTitle.checkedOut = false;
+        bookTitle.dueDate = " ";
+        bookShelf.pop(findBookOnBookshelf(ID, title))
+        printTransaction("Return", findPatron(ID), bookTitle.Title, fee)
+        saveLibrary()
+        savePatrons()
+    }
 }
